3628282453231a68de2f5868881fb533bd0af6b2
Vassilis Markos <51245808+VMarkos@users.noreply.github.com>
2021-09-07 13:56:07 +0300

    Update color picking methodology.

--- colourPicker.js
+++ colourPicker.js
@@ -14,18 +14,31 @@ function initColourPickerLine() {
     context.fillRect(0, 0, width, height);
 }
 
-function initColourPanel() {
+function initColourPanel(color) {
     const width = document.getElementById("color-panel").width;
     const height = document.getElementById("color-panel").height;
     const context = document.getElementById("color-panel").getContext("2d");
     const horizontalGradient = context.createLinearGradient(0, 0, width, 0);
     horizontalGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-    horizontalGradient.addColorStop(1, "rgba(255, 0, 0, 1)");
+    horizontalGradient.addColorStop(1, "rgba(" + color + ", 1)");
     context.fillStyle = horizontalGradient;
     context.fillRect(0, 0, width, height);
     const verticalGradient = context.createLinearGradient(0, 0, 0, height);
-    verticalGradient.addColorStop(0, "rgba(255, 0, 0, 0)");
+    verticalGradient.addColorStop(0, "rgba(" + color + ", 0)");
     verticalGradient.addColorStop(1, "rgba(0, 0, 0, 1)");
     context.fillStyle = verticalGradient;
     context.fillRect(0, 0, width, height);
+}
+
+function updateUserColor() {
+    const x = document.getElementById("color-panel-picker").offsetLeft;
+    const y = document.getElementById("color-panel-picker").offsetTop;
+    const panelPickerContainer = document.getElementById("color-panel-container");
+    const parentWidth = panelPickerContainer.getBoundingClientRect().width;
+    const parentHeight = panelPickerContainer.getBoundingClientRect().height;
+    const panelColorLeft = 300 * (x) / (parentWidth - 20);
+    const panelColorTop = 150 * (y) / (parentHeight - 20);
+    let imageData = document.getElementById("color-panel").getContext("2d").getImageData(panelColorLeft, panelColorTop, 1, 1);
+    document.getElementById("user-head").style.backgroundColor = `rgb(${imageData.data[0]}, ${imageData.data[1]}, ${imageData.data[2]})`;
+    document.getElementById("user-body").style.backgroundColor = `rgb(${imageData.data[0]}, ${imageData.data[1]}, ${imageData.data[2]})`;
 }
