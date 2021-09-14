e12bb76684c32f5df1897ad780b0aa163389e96d
Vassilis Markos <51245808+VMarkos@users.noreply.github.com>
2021-09-14 19:52:48 +0300

    Update window resizing effects.

--- utils.js
+++ utils.js
@@ -1,6 +1,15 @@
 function onLoad() {
     initColourPickerLine();
     initColourPanel("255,0,0");
+    if (window.localStorage.getItem("hasVisited") === null) {
+        window.localStorage.setItem("hasVisited", "false");
+    }
+    if (window.localStorage.getItem("hasVisited") === "false") {
+        document.getElementById("tour-popup").style.visibility = "visible";
+        localStorage.setItem("hasVisited", "true");
+    }
+    const windowWidth = document.body.clientWidth;
+    document.getElementById("body-container").style.minWidth = windowWidth + "px";
 }
 
 function diversityCoefficient() {
