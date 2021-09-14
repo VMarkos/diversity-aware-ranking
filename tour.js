69eb984d91164ba27db1074ee19a43ede9bb1cca
Vassilis Markos <51245808+VMarkos@users.noreply.github.com>
2021-09-14 19:53:44 +0300

    Update tour functionality.

--- tour.js
+++ tour.js
@@ -1,5 +1,6 @@
 function startTour() {
-    document.getElementById("tour-1").style.boxShadow = "0 0 0 200vmax rgba(0, 0, 0, 0.6)";
+    this.document.getElementById("tour-popup").style.visibility = "hidden";
+    document.getElementById("tour-1").style.boxShadow = "0 0 0 200vmax rgba(0, 0, 0, 0.8)";
     document.getElementById("tour-1").style.zIndex = 1;
     document.getElementById("help-sidebar-1").style.visibility = "visible";
 }
@@ -10,7 +11,7 @@ function tourStep(step) {
     document.getElementById("tour-" + previous).style.boxShadow = "";
     document.getElementById("tour-" + previous).style.zIndex = 0;
     document.getElementById("tour-" + step).style.zIndex = 1;
-    document.getElementById("tour-" + step).style.boxShadow = "0 0 0 200vmax rgba(0, 0, 0, 0.6)";
+    document.getElementById("tour-" + step).style.boxShadow = "0 0 0 200vmax rgba(0, 0, 0, 0.8)";
     document.getElementById("help-sidebar-" + step).style.visibility = "visible";
 }
 
@@ -18,4 +19,8 @@ function tourEnd() {
     document.getElementById("help-sidebar-6").style.visibility = "hidden";
     document.getElementById("tour-6").style.boxShadow = "";
     document.getElementById("tour-6").style.zIndex = 0;
+}
+
+function skipTour() {
+    document.getElementById("tour-popup").style.visibility = "hidden";
 }
